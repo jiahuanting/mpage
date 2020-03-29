b64f8f1829f400fcb18b35fd05f07c91a9a447df
jiahuanting <jht1995@126.com>
2020-03-30 00:24:59 +0800

    0330

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,54 @@
 <template>
-  <div class="header">
-    <div class="bigscity_logo">
-      <a href="https://www.bigscity.com/" target="_blank">
-        <img src="static/images/bigscitylogo.png" alt="BIGSCITYLOGO" width="75%">
-      </a>
-    </div>
-    <div class="menu">
-      <ul>
-        <li>
-          <a href="#">Epidemic Forecast</a>
-          <ul>
-            <li><a href="/#/forecasting">English</a></li>
-            <li><a href="/#/forecasting_cn">Chinese</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="#">Economic Recovery</a>
-          <ul>
-            <li><a href="/#/resumption">English</a></li>
-            <li><a href="/#/resumption_cn">Chinese</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="/#/r-value">Weather Effects</a>
-        </li>
-        <li><a href="https://github.com/bigscity/nCov-predict" target="_blank">Data</a></li>
-        <li><a href="https://www.bigscity.com/" target="_blank">BIGSCITY</a></li>
-      </ul> 
-    </div>
-    <div class="title">
-      <div class="buaa_logo">
-        <a href="https://www.buaa.edu.cn/" target="_blank">
-          <img src="static/images/buaalogo.png" alt="BUAALOGO" width="80%">
+  <div id="main">
+    <div class="header">
+      <div class="logo">
+        <a href="https://www.bigscity.com/" target="_blank">
+          <img src="static/images/bigscitylogo.png" alt="BIGSCITYLOGO">
         </a>
       </div>
-      <div class="text">
-        <p style="font-weight: 100; font-size: 14px;">Coronavirus (COVID-19)</p>
-        <p style="font-weight: 100; font-size: 14px;">Research team</p>
+      <div class="left">
+        <div class="menu">
+          <ul>
+            <li>
+              <a href="#">Epidemic Forecast</a>
+              <ul>
+                <li><a href="/#/forecasting">English</a></li>
+                <li><a href="/#/forecasting_cn">Chinese</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Economic Recovery</a>
+              <ul>
+                <li><a href="/#/resumption">English</a></li>
+                <li><a href="/#/resumption_cn">Chinese</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="/#/r-value">Weather Effects</a>
+            </li>
+            <li><a href="https://github.com/bigscity/nCov-predict" target="_blank">Data</a></li>
+            <li><a href="https://www.bigscity.com/" target="_blank">BIGSCity Lab</a></li>
+          </ul> 
+        </div>
+      </div>
+      <div class="right">
+        <div class="panel">
+          <div class="buaa">
+            <a href="https://www.buaa.edu.cn/" target="_blank">
+              <img src="static/images/buaalogo.png" alt="BUAALOGO" width="100%">
+            </a>
+          </div>
+          <div class="text">
+            <p style="font-weight: 100; font-size: 14px;">Beihang University</p>
+            <p style="font-weight: 100; font-size: 14px;">COVID-19 Research Team</p>
+          </div>
+        </div>
       </div>
     </div>
     <router-view></router-view>
   </div>
+    
+    
 </template>
 
 <script>
@@ -57,41 +65,82 @@ export default {
 </script>
 
 <style>
-  html, body, div, p, h4 {
+  html, body, div, p {
     padding: 0;
     margin: 0;
   }
-  .header {
-    height: auto;
+  html, body {
+    width: 100%;
+    height: 100%;
     background-color: #0065B3;
-    display: flex;
-    flex-wrap: nowrap;
   }
-  .bigscity_logo {
-    margin-left: 1%;
+  #main {
+    position: absolute;
+
+    width: 100%;
+    /* height: calc(100% - 60px); */
+    height: 100%;
+  }
+  .header {
+    width: 100%;
+    height: 60px;
+    /* min-height: 60px; */
+    background-color: #0065B3;
+    z-index: 100;
+    font-family: "Arial";
+  }
+  .logo {
+    float: left;
     width: 5%;
+    height: 100%;
   }
-  .menu {
-    width: 55%;
+  .logo img {
+    padding: 4%;
+    width: 90%;
+    height: 100%;
+  }
+  .left {
+    float: left;
+    width: 70%;
+    height: 100%;
+    font-size: 18px;
+  }
+  .right {
+    float: right;
+    width: 25%;
+    height: 100%;
+  }
+  .panel {
+    float: right;
+    width: 100%;
+    height: 100%;
     display: flex;
     align-items: center;
   }
-  .title {
-    width: 40%;
-    display: flex;
-    align-items: center;
-  }
-  .buaa_logo {
-    margin-left: auto;
+  .buaa {
+    float: left;
+    width: 50%;
+    height: 85%;
     text-align: right;
-    width: 40%;
     border-right: 1px solid #fff;
   }
+  .buaa img {
+    padding: 3%;
+    width: 100%;
+    height: 60%;
+  }
   .text {
-    margin-left: 20px;
-    text-align: left;
-    width: 40%;
+    float: left;
+    width: 50%;
+    padding-left: 10px;
     color: #fff;
+  }
+  .menu {
+    float: left;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
   }
   ul{
     width: 100%;
@@ -104,7 +153,7 @@ export default {
   }
   li {
     float: left; /* 使li内容横向浮动，即横向排列 */
-    padding-right: 2%;  /* 两个li之间的距离 */
+    padding-right: 1%;  /* 两个li之间的距离 */
     position: relative;
     overflow: hidden;
   }
@@ -112,18 +161,16 @@ export default {
     display: block; /* 把链接显示为块元素可使整个链接区域可点击 */
     color:white;
     text-align: center;
-    padding: 5px;
+    padding: 10px 10px;
     overflow: hidden;
     text-decoration: none; /* 去除下划线 */
-    font-weight: 500;
+    font-weight: 600;
   }
   ul li ul{
-    /* 设置二级菜单 */
     width: 100px;
     margin: 0px;
     background: #0065B3;
     position: fixed;
-    z-index: 100;
     display: none; /* 默认隐藏二级菜单的内容 */
   }
   li a:hover{
@@ -140,7 +187,6 @@ export default {
   ul li ul li {
     padding: 0px;
     margin: 0px;
-    /* 二级菜单li内容的显示 */
     float: none;
     text-align: center;
   }
@@ -148,10 +194,7 @@ export default {
     display: block;
   }
   #show-iframe {
-    position: absolute;
-    z-index: 10;
-    height: calc(100% - 60px);
-    top: 60px;
     width: 100%;
+    height: calc(100% - 60px);
   }
 </style>
